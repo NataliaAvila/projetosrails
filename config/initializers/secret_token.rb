16db6bcca282a92181e6640caafc9a86369a2304
Natalia Avila <natalia.avilabh@gmail.com>
2013-03-05 13:55:20 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projetosrails::Application.config.secret_token = 'd2263f51da83109fe82136145f68e83d28e5039fa8ade9568dbe30bd0c4ed8ff146913c5704ae0aff0120b4fd38f790d72888b5e794f42b045e2fd1db1ea73f6'
